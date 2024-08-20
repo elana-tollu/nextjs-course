286d50d247f815e10cf7fe3d9fe1565b82c39311
Elana Tollu <elana.tollu@yandex.ru>
2024-08-20 17:03:27 +0300

    open modal with button

--- react-refresher/src/App.jsx
+++ react-refresher/src/App.jsx
@@ -1,11 +1,30 @@
-import './App.css';
+import { useState } from 'react';
+
 import PostsList from './components/PostsList';
+import MainHeader from './components/MainHeader';
+import './App.css';
 
 function App() {
+  const [modalIsVisible, setModalIsVisible] = useState(false);
+  
+  function showModalHandler() {
+    setModalIsVisible(true);
+  }
+
+  function hideModalHandler() {
+    setModalIsVisible(false);
+  }
+    
   return( 
-    <main>
-      <PostsList />
-    </main>
+    <>
+      <MainHeader onCreatePost={showModalHandler} />
+      <main>
+        <PostsList 
+          isPosting={modalIsVisible}
+          onStopPosting={hideModalHandler} 
+        />
+      </main>
+    </>
   );
 }
 
--- react-refresher/src/components/PostsList.jsx
+++ react-refresher/src/components/PostsList.jsx
@@ -5,15 +5,10 @@ import Post from './Post';
 import Modal from './Modal';
 import styles from './PostsList.module.css';
 
-function PostsList () {
-    const [modalIsVisible, setModalIsVisible] = useState(true);
+function PostsList ({isPosting, onStopPosting}) {
     const [enteredBody, setEnteredBody] = useState('');
     const [enteredAuthor, setEnteredAuthor] = useState('');
-
-    function hideModalHandler() {
-        setModalIsVisible(false);
-    }
-
+    
     function bodyChangeHandler(event) {
         setEnteredBody(event.target.value);
     }
@@ -24,14 +19,14 @@ function PostsList () {
 
     return (
         <>
-            { modalIsVisible ? 
-                <Modal onClose={hideModalHandler}>
+            { isPosting ? 
+                <Modal onClose={onStopPosting}>
                     <NewPost 
                         onBodyChange={bodyChangeHandler} 
                         onAuthorChange={authorChangeHandler} 
                     />
                 </Modal>
-                : null}
+                : null }
             <ul className={styles.posts}>
                 <Post author={enteredAuthor} body={enteredBody} />
                 <Post author='Ilia' body='Check out the full course!!!'/>
